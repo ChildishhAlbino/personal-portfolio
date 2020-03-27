ecfc2049ef8f5fbe6321af64af8ab06c66d98cfc
Connor <connorwilliams.home@gmail.com>
2020-03-28 04:26:23 +1100

    chore(add-separator): Add separator between bio and blog posts.

--- src/pages/index.js
+++ src/pages/index.js
@@ -18,6 +18,7 @@ class BlogIndex extends React.Component {
       <Layout location={this.props.location} title={siteTitle}>
         <SEO title="All posts" />
         <Bio />
+        <hr />
         {posts.map(({ node }) => {
           const title = node.frontmatter.title || node.fields.slug
           return (
