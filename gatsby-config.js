bd90e87c16ea94b5cf6f013ac120a730959acf76
Connor <connorwilliams.home@gmail.com>
2020-03-28 02:10:16 +1100

    chore(update-site-icon): Change Icon to me!

--- gatsby-config.js
+++ gatsby-config.js
@@ -69,7 +69,7 @@ module.exports = {
         background_color: `#ffffff`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `content/assets/gatsby-icon.png`,
+        icon: `content/assets/profile-pic-circle.png`,
       },
     },
     `gatsby-plugin-react-helmet`,
