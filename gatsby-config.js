daa8d99b84b1c7ea5feb093b4d25f310cf1b5932
Connor <connorwilliams.home@gmail.com>
2020-03-28 13:58:09 +1100

    chore(add-blurb-fs): Add fs config for /blurb folder.

--- gatsby-config.js
+++ gatsby-config.js
@@ -38,6 +38,13 @@ module.exports = {
       },
     },
     {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/content/blurb`,
+        name: `blurb`,
+      },
+    },
+    {
       resolve: `gatsby-transformer-remark`,
       options: {
         plugins: [
