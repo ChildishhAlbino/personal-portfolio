cee530a085d4bff25a12b5cc3ca41ad476bbdb67
Connor <connorwilliams.home@gmail.com>
2020-03-30 14:01:00 +1100

    chore(plugins): Update mainfest plugin with correct values.

--- gatsby-config.js
+++ gatsby-config.js
@@ -84,10 +84,10 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `Gatsby Starter Blog`,
-        short_name: `GatsbyJS`,
+        name: `childishhalbino.com`,
+        short_name: `ChilidishhAlbino`,
         start_url: `/`,
-        background_color: `#ffffff`,
+        background_color: `#303030`,
         theme_color: `#663399`,
         display: `minimal-ui`,
         icon: `content/assets/profile-pic-circle.png`,
