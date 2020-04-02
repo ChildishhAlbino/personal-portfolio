d2b06e941a9a3a436a48ff264a08be88cc8d79ba
Connor <connorwilliams.home@gmail.com>
2020-04-02 17:04:47 +1100

    feature(about-page): Add placeholder for about page.

---
+++ src/pages/about.js
@@ -0,0 +1,30 @@
+import React from "react"
+import { graphql } from "gatsby"
+import Bio from "../components/bio/bio"
+import Layout from "../components/layout/layout"
+import SEO from "../components/seo/seo"
+class AboutPage extends React.Component {
+    render() {
+        const { data } = this.props
+        const siteTitle = data.site.siteMetadata.title
+        return (
+            <Layout location={this.props.location} title={siteTitle}>
+                <SEO title="All posts" />
+                <Bio />
+                <hr />
+            </Layout>
+        )
+    }
+}
+
+export default AboutPage
+
+export const pageQuery = graphql`
+query {
+    site {
+        siteMetadata {
+            title
+        }
+    }
+}
+`
