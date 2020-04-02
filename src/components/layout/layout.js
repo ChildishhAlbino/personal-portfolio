95a6804c77cf6a9fed55a71a265cbf1ceff5c079
Connor <connorwilliams.home@gmail.com>
2020-04-02 17:09:42 +1100

    chore(header-changes): Streamline header size across all pages.

--- src/components/layout/layout.js
+++ src/components/layout/layout.js
@@ -9,26 +9,14 @@ class Layout extends React.Component {
   render() {
     const { location, title, children } = this.props
     const rootPath = `${__PATH_PREFIX__}/`
-    let header
-    if (location.pathname === rootPath) {
-      header = (
-        <h1>
-          <Link to={`/`}>{title}</Link>
-        </h1>
-      )
-    } else {
-      header = (
-        <h3>
-          <Link to={`/`}>{title}</Link>
-        </h3>
-      )
-    }
     return (
       <div className="layout-wrapper">
         <div className="layout-container">
           <header className="header">
             <script src="https://identity.netlify.com/v1/netlify-identity-widget.js"></script>
-            {header}
+            <h1>
+              <Link to={`/`}>{title}</Link>
+            </h1>
             <Navbar></Navbar>
             <ThemeToggler>
               {({ theme, toggleTheme }) => (
