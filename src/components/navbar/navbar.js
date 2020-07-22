d01c1ddc81bf9ef6aaa542050ac2837a80a6b9aa
Connor <connorwilliams.home@gmail.com>
2020-07-23 00:06:27 +1000

    chore: Change links page to projects page.

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -8,7 +8,7 @@ const NavBar = () => {
         <div className="navbar-container">
             <Link className="navbar-item" activeClassName="active-navbar-item" to="/work"><b>Work</b></Link>
             <Link className="navbar-item" activeClassName="active-navbar-item" to="/about"><b>About</b></Link>
-            <Link className="navbar-item" activeClassName="active-navbar-item" to="/links"><b>Links</b></Link>
+            <Link className="navbar-item" activeClassName="active-navbar-item" to="/projects"><b>Projects</b></Link>
         </div>
     )
 }
--- src/pages/projects.js
+++ src/pages/projects.js
@@ -4,14 +4,14 @@ import Bio from "../components/bio/bio"
 import Layout from "../components/layout/layout"
 import SEO from "../components/seo/seo"
 
-class LinksPage extends React.Component {
+class ProjectsPage extends React.Component {
     render() {
         const { data } = this.props
         const siteTitle = data.site.siteMetadata.title
         return (
             <Layout location={this.props.location} title={siteTitle}>
-                <SEO title="Links" />
-                <h1>Links</h1>
+                <SEO title="Projects" />
+                <h1>Projects</h1>
                 <small>Work in Progress...</small>
                 <hr />
                 <Bio />
@@ -20,7 +20,7 @@ class LinksPage extends React.Component {
     }
 }
 
-export default LinksPage
+export default ProjectsPage
 
 export const pageQuery = graphql`
 query {
