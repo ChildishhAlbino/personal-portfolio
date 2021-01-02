ffc56f56c404ff5c873201d99bdf702f80d913c4
Connor <connorwilliams.home@gmail.com>
2021-01-02 23:25:20 +1100

    update: remove old node packages.

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,6 +1,4 @@
 const path = require(`path`)
-const { createFilePath } = require(`gatsby-source-filesystem`)
-const { fmImagesToRelative } = require(`gatsby-remark-relative-images`)
 
 exports.createPages = async ({ graphql, actions }) => {
   const { createPage } = actions
