51e7ab9322c53a3afdfd8ee2ba9c86c9e927f8cb
Connor <connorwilliams.home@gmail.com>
2022-07-30 00:30:52 +1000

    Add prettier and formatting

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 # childishhalbino.com
+
 A website/blog built in GatsbyJS. Uses Netlify for deployment and as a CMS.
 
 [![Netlify Status](https://api.netlify.com/api/v1/badges/09078d53-2b95-49fd-8abe-b99d7a4e7fd3/deploy-status)](https://app.netlify.com/sites/childishhalbino/deploys)
-
 
 # Create T3 App
 
--- src/utils/trpc.ts
+++ src/utils/trpc.ts
@@ -26,17 +26,17 @@ export const trpc = setupTRPC<AppRouter>({
  * @example type HelloOutput = inferQueryOutput<'hello'>
  */
 export type inferQueryOutput<
-  TRouteKey extends keyof AppRouter["_def"]["queries"],
+  TRouteKey extends keyof AppRouter["_def"]["queries"]
 > = inferProcedureOutput<AppRouter["_def"]["queries"][TRouteKey]>;
 
 export type inferQueryInput<
-  TRouteKey extends keyof AppRouter["_def"]["queries"],
+  TRouteKey extends keyof AppRouter["_def"]["queries"]
 > = inferProcedureInput<AppRouter["_def"]["queries"][TRouteKey]>;
 
 export type inferMutationOutput<
-  TRouteKey extends keyof AppRouter["_def"]["mutations"],
+  TRouteKey extends keyof AppRouter["_def"]["mutations"]
 > = inferProcedureOutput<AppRouter["_def"]["mutations"][TRouteKey]>;
 
 export type inferMutationInput<
-  TRouteKey extends keyof AppRouter["_def"]["mutations"],
+  TRouteKey extends keyof AppRouter["_def"]["mutations"]
 > = inferProcedureInput<AppRouter["_def"]["mutations"][TRouteKey]>;
