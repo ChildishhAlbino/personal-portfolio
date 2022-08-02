fcbb94229f673e555157b2d2d0e21213f8083270
Connor <connorwilliams.home@gmail.com>
2022-08-03 01:47:51 +1000

    chore: make 404 page look nice

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,9 +1,13 @@
 function NotFoundPage() {
   return (
     <>
-      <h1>404</h1>
-      <p>This is not the page you&apos;re looking for! 👋</p>
-      <p>But it&apos;s the only one you&apos;ll get cos this path 404&apos;d</p>
+      <span className={'prose prose-xl'}>
+        <h1>404</h1>
+        <p>This is not the page you&apos;re looking for! 👋</p>
+        <p>
+          But it&apos;s the only one you&apos;ll get cos this path 404&apos;d
+        </p>
+      </span>
     </>
   )
 }
