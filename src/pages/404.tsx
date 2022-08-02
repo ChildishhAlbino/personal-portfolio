fa8fa12edcea6d8833a9200c1c8253ef102cd58c
Connor <connorwilliams.home@gmail.com>
2022-08-03 01:40:41 +1000

    chore: add 404 page

---
+++ src/pages/404.tsx
@@ -0,0 +1,9 @@
+export default function () {
+  return (
+    <>
+      <h1>404</h1>
+      <p>This is not the page you're looking for! 👋</p>
+      <p>But it's the only one you'll get cos this path 404'd</p>
+    </>
+  )
+}
