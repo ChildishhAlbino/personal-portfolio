0e65f8b2b7854d5f995e1241b90c3a1531104daa
Connor <connorwilliams.home@gmail.com>
2023-03-28 11:58:55 +1100

    feature: favicon

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,10 +4,14 @@ import type { AppType } from 'next/dist/shared/lib/utils'
 import { api } from '../utils/api'
 import Layout from '../components/layout'
 import ContentLayout from '@/components/content-layout'
+import Head from 'next/head'
 
 const MyApp: AppType = ({ Component, pageProps }) => {
   return (
     <Layout>
+      <Head>
+        <link rel="shortcut icon" href="favicon-32x32.png" />
+      </Head>
       <Component {...pageProps} />
     </Layout>
   )
