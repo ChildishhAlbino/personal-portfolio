1fe692ea9c7913d13cd215b0104e1ef2ce529419
Connor <connorwilliams.home@gmail.com>
2023-03-30 01:08:28 +1100

    chore: move letters to components

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,6 @@
 import type { NextPage } from 'next'
 import Link from 'next/link'
-import { Letters } from '../Letters'
+import { Letters } from '../components/Letters'
 const Home: NextPage = () => {
     return (
         <main className='grid h-full grid-cols-1 grid-rows-3 place-items-center px-[2rem]'>
