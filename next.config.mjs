4c30732eb11c06b2e7817fba928cd761754d4e32
Connor <connorwilliams.home@gmail.com>
2023-04-07 00:09:29 +1000

    fix: update env var validation

--- next.config.mjs
+++ next.config.mjs
@@ -1,21 +1,24 @@
-// @ts-check
-
 /**
- * Don't be scared of the generics here.
- * All they do is to give us autocompletion when using this.
- *
- * @template {import("next").NextConfig} T
- * @param {T} config - A generic parameter that flows through to the return type
- * @constraint {{import("next").NextConfig}}
+ * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation.
+ * This is especially useful for Docker builds.
  */
-function defineNextConfig(config) {
-  return config;
-}
+!process.env.SKIP_ENV_VALIDATION && (await import("./src/env.mjs"));
 
-export default defineNextConfig({
+/** @type {import("next").NextConfig} */
+const config = {
   reactStrictMode: true,
-  swcMinify: true,
   images: {
     domains: ["images.ctfassets.net"]
-  }
-});
+  },
+  /**
+   * If you have the "experimental: { appDir: true }" setting enabled, then you
+   * must comment the below `i18n` config out.
+   *
+   * @see https://github.com/vercel/next.js/issues/41980
+   */
+  i18n: {
+    locales: ["en"],
+    defaultLocale: "en",
+  },
+};
+export default config;
