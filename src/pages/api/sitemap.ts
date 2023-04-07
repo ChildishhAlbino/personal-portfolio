3866ea430637ec8965649805549f90f46e09999b
Connor <connorwilliams.home@gmail.com>
2023-04-07 19:11:53 +1000

    chore: refactor sitemap code

--- src/pages/api/sitemap.ts
+++ src/pages/api/sitemap.ts
@@ -19,22 +19,24 @@ async function fetchPostUrlData() {
     });
 }
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-
-    res.statusCode = 200
-    res.setHeader('Content-Type', 'application/xml')
-    // Instructing the Vercel edge to cache the file
-    res.setHeader('Cache-control', 'stale-while-revalidate, s-maxage=3600')
+async function generateXmlSitemap() {
     const [DYNAMIC_PAGES, POSTS] = await Promise.all([fetchDynamicPagesUrlData(), fetchPostUrlData()])
     const BASE_PAGES = [...KNOWN_PAGES, ...DYNAMIC_PAGES]
     const BASE_PAGE_XML = BASE_PAGES.map(generateXMLForBasePage).join("\n")
     const POSTS_XML = POSTS.map(generateXMLForEditablePage).join("\n")
-    const xml = `<?xml version="1.0" encoding="UTF-8"?>
+    return `<?xml version="1.0" encoding="UTF-8"?>
     <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"> 
         ${BASE_PAGE_XML}
         ${POSTS_XML}
-    </urlset>`
+    </urlset>`;
+}
 
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+    const xml = await generateXmlSitemap();
+    res.statusCode = 200
+    res.setHeader('Content-Type', 'application/xml')
+    // Instructing the Vercel edge to cache the file
+    res.setHeader('Cache-control', 'stale-while-revalidate, s-maxage=3600')
     res.end(xml)
 }
 
