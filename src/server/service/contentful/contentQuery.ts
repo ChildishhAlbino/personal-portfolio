62c5907933a28e3a35b5f4a11a38f63240bc09df
Connor <connorwilliams.home@gmail.com>
2023-04-10 12:37:29 +1000

    chore: remove contentQuery log

--- src/server/service/contentful/contentQuery.ts
+++ src/server/service/contentful/contentQuery.ts
@@ -12,7 +12,6 @@ export async function contentQuery<K, T>({
     const isPreviewEnabled = Boolean(
         JSON.parse(CONTENTFUL_PREVIEW_ENABLED || 'false')
     )
-    console.log('NEW REQUEST')
     const res = await fetch(
         `${CONTENTFUL_BASE_ENDPOINT}content/v1/spaces/${CONTENTFUL_SPACE_ID}/environments/master`,
         {
@@ -33,7 +32,6 @@ export async function contentQuery<K, T>({
     if (!!json.errors) {
         throw Error(`Error with query... ${json.errors[0].message}`)
     }
-    console.log('END REQUEST')
     return json.data
 }
 
