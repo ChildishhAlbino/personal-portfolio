741077e6abf99d7fe06d3e2f57bc10ee9254f2bf
Connor <connorwilliams.home@gmail.com>
2023-04-10 11:04:49 +1000

    chore: clean up posts and layout a touch

--- src/components/layouts/page-layout.tsx
+++ src/components/layouts/page-layout.tsx
@@ -1,16 +1,20 @@
+import Head from 'next/head'
 import { ReactNode } from 'react'
 import { FaGithub, FaLinkedin, FaTwitter } from 'react-icons/fa'
 
 export default function PageLayout({ children, header }: ContentLayoutProps) {
     return (
         <>
+            <Head>
+                <title>{`${header} | ChildishhAlbino`}</title>
+            </Head>
             <main
                 id='page-container'
                 className={`mx-auto min-h-full flex max-w-[clamp(600px,_60vw,_1200px)] flex-col mobile:px-4 laptop:px-0`}
             >
                 {header && (
                     <header className='flex w-full justify-between items-center text-res-title-md font-bold uppercase border-b-2 border-indigo-600'>
-                        <h1>{header}</h1>
+                        <h1>{header}:</h1>
 
                         <div className='flex gap-4'>
                             <a
