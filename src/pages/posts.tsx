23fd14fa4cbdab5076967399c8ed27bda9e99eea
ChildishhAlbino <connorwilliams.home@gmail.com>
2023-06-09 02:03:19 +1000

    feat: make post aggregation page static

--- src/pages/posts.tsx
+++ src/pages/posts.tsx
@@ -1,8 +1,9 @@
-import type { NextPage } from 'next'
+import type { GetStaticPropsContext, NextPage } from 'next'
 import { api } from '@/utils/api'
 import PageLayout from '@/components/layouts/page-layout'
-import { DateTime } from 'luxon'
-
+import { createServerSideHelpers } from '@trpc/react-query/server'
+import superjson from 'superjson'
+import { appRouter } from '@/server/api/root'
 import { SlugAggregation } from '@/types/post'
 import {
     DynamicPostAggregationItem,
@@ -59,3 +60,33 @@ function LoadingPostAggregationList() {
     )
 }
 
+
+type PostsPageProps = {}
+
+
+export async function getStaticProps({}: GetStaticPropsContext<PostsPageProps>) {
+    const ssg = await createServerSideHelpers({
+        router: appRouter,
+        ctx: {},
+        transformer: superjson, // optional - adds superjson serialization
+    })
+
+    try {
+        await ssg.contentful.getPostSlugs.fetch({})
+    } catch (e: any) {
+        const cause = e.cause
+        if (cause.message.includes('No page for slug')) {
+            return {
+                notFound: true,
+            }
+        }
+
+        throw e
+    }
+    return {
+        props: {
+            trpcState: ssg.dehydrate(),
+        },
+        revalidate: 30,
+    }
+}
