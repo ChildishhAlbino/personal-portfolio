ecc5074b4e65c09d6bc093f0d8937e61a5440501
ChildishhAlbino <connorwilliams.home@gmail.com>
2023-06-12 12:42:57 +1000

    fix: build

--- src/components/post-aggregation-item.tsx
+++ src/components/post-aggregation-item.tsx
@@ -1,81 +1,13 @@
 import { PostAggregation, SlugAggregation } from '@/types/post'
 import PostThumbnail from '@/components/post-thumbnail'
 import Link from 'next/link'
-
 import { Skeleton } from '@/components/ui/skeleton'
-import { api } from '@/utils/api'
 import { DateTime } from 'luxon'
 
 function formatPublicationDateInline(publicationDate: string) {
     return DateTime.fromISO(publicationDate)
         .setLocale('en-AU')
         .toLocaleString(DateTime.DATETIME_FULL)
-}
-
-export function DynamicPostAggregationItem({ slug }: { slug: SlugAggregation }) {
-
-    const postQuery = api.contentful.getPostAggregationBySlug.useQuery({ slug: slug.slug })
-
-    const { data, isLoading, error } = postQuery
-
-    if (isLoading) {
-        return <LoadingPostAggregationItem />
-    }
-
-    if (!data || error) {
-        throw error || Error("Whoops!")
-    }
-
-    const [rawPost] = data.posts
-    if (!rawPost) {
-        throw Error("Somehow no posts were returned...")
-    }
-    const item = formatPublicationDate(rawPost)
-    const topKeywords = item.keywords.slice(0, 5)
-    const totalKeywords = topKeywords.length
-    const thumbnailProps = {
-        ...item.thumbnail,
-        fixedMaxHeight: 800,
-    }
-    return (
-        <>
-            <div
-                className='grid w-full border-b-2 border-light border-opacity-30 pb-4 mobile:grid-cols-1 mobile:gap-y-8 mobile:text-center desktop:grid-cols-[2fr,_5fr] desktop:gap-x-4 desktop:text-left'>
-                <PostThumbnail {...thumbnailProps} />
-                <span>
-                    <Link
-                        href={`/posts/${item?.slug}`}
-                        className='text-res-title-sm text-text underline'
-                    >
-                        <h1>{item?.title}</h1>
-                    </Link>
-                    <div className='flex flex-col gap-1'>
-                        <i className='break-words'>{item?.description}</i>
-                        <br />
-                        <pre className='text-sm'>
-                            {item?.publicationDate} <small>UTC+11</small>
-                        </pre>
-                        <span
-                            className='flex flex-wrap gap-4 text-sm mobile:justify-center mobile:self-center desktop:self-start'>
-                            {topKeywords.map((keyword, index) => {
-                                const suffix =
-                                    index < totalKeywords - 1 ? ',' : ''
-                                return (
-                                    <i
-                                        className='cursor-pointer text-text-darker underline'
-                                        key={`${keyword}_${index}`}
-                                    >
-                                        #{keyword}
-                                        {suffix}
-                                    </i>
-                                )
-                            })}
-                        </span>
-                    </div>
-                </span>
-            </div>
-        </>
-    )
 }
 
 export function PostAggregationItem({ post }: { post: PostAggregation }) {
--- src/pages/posts.tsx
+++ src/pages/posts.tsx
@@ -6,18 +6,11 @@ import superjson from 'superjson'
 import { appRouter } from '@/server/api/root'
 import { PostAggregation, SlugAggregation } from '@/types/post'
 import {
-    DynamicPostAggregationItem,
     LoadingPostAggregationItem,
     PostAggregationItem,
 } from '@/components/post-aggregation-item'
 
 const PostsAggregationPage: NextPage = () => {
-    // const slugQuery = api.contentful.getPostSlugs.useQuery({})
-
-    // const { data, isLoading, error } = slugQuery
-
-    // const { slugs } = data || { slugs: [] }
-
     const postsQuery = api.contentful.getPosts.useQuery({})
 
     const { data, isLoading, error } = postsQuery
@@ -35,9 +28,7 @@ const PostsAggregationPage: NextPage = () => {
         >
             <span className='flex h-full flex-col gap-y-4'>
                 <span>
-                    {showLoader && <LoadingPostAggregationList />}
                     {showError && <h1> ERROR </h1>}
-                    {/* {showPosts && <DynamicPostAggregationList slugs={slugs} />} */}
                     {showPosts && <PostAggregationList posts={posts} />}
                 </span>
             </span>
@@ -47,22 +38,6 @@ const PostsAggregationPage: NextPage = () => {
 
 export default PostsAggregationPage
 
-function DynamicPostAggregationList({ slugs }: { slugs: SlugAggregation[] }) {
-    return (
-        <div className='flex flex-col gap-y-8'>
-            {slugs &&
-                slugs.map((item) => {
-                    return (
-                        <DynamicPostAggregationItem
-                            slug={item}
-                            key={item.slug}
-                        />
-                    )
-                })}
-        </div>
-    )
-}
-
 function PostAggregationList({ posts }: { posts: PostAggregation[] }) {
     return (
         <div className='flex flex-col gap-y-8'>
@@ -70,17 +45,6 @@ function PostAggregationList({ posts }: { posts: PostAggregation[] }) {
                 posts.map((post) => {
                     return <PostAggregationItem post={post} key={post.slug} />
                 })}
-        </div>
-    )
-}
-
-function LoadingPostAggregationList() {
-    const items = Array(12).fill(0)
-    return (
-        <div className='flex flex-col gap-y-8'>
-            {items.map((_, index) => {
-                return <LoadingPostAggregationItem key={index} />
-            })}
         </div>
     )
 }
