9e4c87b935b98d41215d57df3333aa722949b1a3
ChildishhAlbino <connorwilliams.home@gmail.com>
2023-06-12 12:54:27 +1000

    chore: change default locale for project

--- next.config.mjs
+++ next.config.mjs
@@ -18,8 +18,8 @@ const config = {
    * @see https://github.com/vercel/next.js/issues/41980
    */
   i18n: {
-    locales: ["en"],
-    defaultLocale: "en"
+    locales: ["en-AU"],
+    defaultLocale: "en-AU"
   },
   async rewrites() {
     return [
